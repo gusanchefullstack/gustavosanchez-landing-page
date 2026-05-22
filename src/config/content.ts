49ab7f9b3d5c89a31aa595263a390571f5ea651f
Gustavo Sanchez Galarza <gustavosanchezgalarza@gmail.com>
2026-05-22 12:35:22 -0700

    Fist blog entry in @hashnode rename title

--- src/config/content.ts
+++ src/config/content.ts
@@ -453,7 +453,7 @@ export const content: SiteContent = {
     items: [
       // Add entries here as you publish posts on Hashnode:
       // { title: "My First Post", url: "https://gusanchedev.hashnode.dev/my-first-post" },
-      { title: "My First Post", url: "https://gustavosanchez.hashnode.dev/my-learning-path-to-software-engineer" },
+      { title: "My learning path to software engineer", url: "https://gustavosanchez.hashnode.dev/my-learning-path-to-software-engineer" },
     ],
   },
 
